72e9e88f714922e5b797493c85a3528e8f574223
Wiktor <w.o.androsiuk@gmail.com>
2019-06-08 22:01:55 +0200

    wyrównany canvas

--- src/Drawing.js
+++ src/Drawing.js
@@ -4,8 +4,6 @@ function Drawing()
 {
 	return <div className="Column">
 		<canvas id="drawingCanvas"/>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet
-		pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique.
 	</div>
 }
 
