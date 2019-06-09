31db087e71fda45be5d8a12e498a4be9f083cf7e
Deathbat2190 <celo1234567890@gmail.com>
2019-06-09 10:20:11 +0200

    Added snap scrolling

--- src/App.js
+++ src/App.js
@@ -6,15 +6,36 @@ import Drawing from "./Drawing";
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>
-					Letters recognition with neural network and k-NN.
-				</p>
-			</header>
 			<div className="Content">
+				<header id="content" className="App-header">
+					<p>
+						Letters recognition with neural network and k-NN.
+					</p>
+				</header>
+
 				<Drawing/>
 				<div className="Column">
 					<Training/>
+				</div>
+				<div className="Column">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet
+					pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique.
+				</div>
+			</div>
+			<div className="clearfix"></div>
+			<div className="Documentation">
+				<header id="documentation" className="App-header">
+					<p>
+						Documentation
+					</p>
+				</header>
+
+				<div className="Column">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet
+					pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique.
+				</div>
+				<div className="Column">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet
+					pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique.
 				</div>
 				<div className="Column">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet
 					pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique.
