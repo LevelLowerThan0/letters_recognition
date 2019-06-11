4f888fcc54fc19fd4092941ba7a5dcda400ecddf
Wiktor <w.o.androsiuk@gmail.com>
2019-06-11 20:36:41 +0200

    Data Load progress

--- src/LoadData.js
+++ src/LoadData.js
@@ -1,4 +1,21 @@
-export function test()
+export function DataLoad()
 {
+    var predicted = [];
+    var values = [];
 
+    const fs = require('fs')
+
+    fs.readFile('letter-recognition.data', (err, data) => {
+        if (err) throw err;
+
+        var lines = data.toString().split("\n");
+
+        lines.forEach( e => {
+            var elements = e.split(",");
+
+            elements.forEach(f => {
+
+            })
+        })
+    });
 }
